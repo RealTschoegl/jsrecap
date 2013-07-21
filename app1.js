20e59e55a165c9e31cd519d5df9c58c0f85de0bb
MakerSquare Student <students@themakersquare.com>
2013-07-21 17:21:18 -0500

    Added JS from Event Handlers

---
+++ app1.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+  // exercise 35
+  // $('.main.card h1').click(function(){
+  //   alert("Weinerschnitzel");
+  // });
+ // exercise 36
+  // $('.main.card h1').click(function(){
+  //   $('.main.card h1').append("!");
+  // });
+ // exercise 37
+  // $('body').click(function() {
+  //   $('body').css("background-color","blue");
+  // });
+ // exercise 38
+  // $('.main.card').click(function(){
+  //   var georgeCloney = $('.main.card').clone();
+  //   $('.main.card').after(georgeCloney);
+  // });
+ // exercise 39 is incomplete 
+  // $('.main.card').click(function(){
+  //   $('body').click(function(){
+  //     $('img.profilepic').hide();
+  //   });
+  // });
+// exercise 40
+  // $('img.profilepic').mousedown(function(){
+  //   $('img.profilepic').hide();
+  // });
+  // $('img.profilepic').mouseup(function(){
+  //   $('img.profilepic').hide();
+  // });
+  // $('img.profilepic').mouseover(function(){
+  //   $('img.profilepic').hide();
+  // });
+  // $('img.profilepic').mouseout(function(){
+  //   $('img.profilepic').hide();
+  // });
+});
+
