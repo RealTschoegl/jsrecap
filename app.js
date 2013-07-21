35eac492ef20f00168521bfe6e13c876f2fb986e
MakerSquare Student <students@themakersquare.com>
2013-07-21 17:17:07 -0500

    Initial commit.

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  
+  function validateForm() {
+    if (document.getElementById.value == null) {
+      alert("Enter out your email address.")
+    };
+  }
+  $(':submit').click(validateForm());
+});
