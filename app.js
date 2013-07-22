09e25c32a6b61b09307725f4097d7199ed77be32
MakerSquare Student <students@themakersquare.com>
2013-07-22 12:05:27 -0500

    Improved form.

--- app.js
+++ app.js
@@ -1,9 +1,81 @@
 $(document).ready(function() {
+  $(':submit').click(function() {
+  	$('div#error_message').empty();
+  	$('div#success_message').empty();
+  	var charFind = document.getElementById("email").value
+  	event.preventDefault();
+  	if (document.getElementById("email").value == "") {
+    	$('div#error_message').append("- Enter your email address.<br>");
+	   } 
+	  if (document.getElementById("password").value == "") {
+    	$('div#error_message').append("- Enter your password.<br>");
+	   } 
+	  if (document.getElementById("email").value.length < 8) {
+    	$('div#error_message').append("- Your email should be at least 8 characters.<br>");
+	   } 
+	  if (document.getElementById("password").value.length < 8) {
+    	$('div#error_message').append("- Your password should be at least 8 characters.<br>");
+	   } 
+	  if (charFind.indexOf("@") == -1) {
+    	$('div#error_message').append("- Your email should contain an '@' symbol.<br>");
+	   }  
+	  if (charFind.indexOf(".") == -1) {
+    	$('div#error_message').append("- Your email should contain an '.' symbol.<br>");
+	   }  
+    return true
+    if (return = true) {
+      $('div#success_message').append("Your email has been sent!");
+    } 
+  });
   
-  function validateForm() {
-    if (document.getElementById.value == null) {
-      alert("Enter out your email address.")
-    };
-  }
-  $(':submit').click(validateForm());
 });
+
+// $(document).ready(function() {
+//   $(':submit').click(function() {
+//   	$('div#error_message').empty();
+//   	event.preventDefault();
+//   	if (document.getElementById("email").value == "") {
+//     	$('div#error_message').append("- Enter your email address.<br>");
+// 	};
+// 	if (document.getElementById("password").value == "") {
+//     	$('div#error_message').append("- Enter your password.<br>");
+// 	};
+// 	if (document.getElementById("email").value.length < 8) {
+//     	$('div#error_message').append("- Your email should be at least 8 characters.<br>");
+// 	};
+// 	if (document.getElementById("password").value.length < 8) {
+//     	$('div#error_message').append("- Your password should be at least 8 characters.<br>");
+// 	};
+// 	var charFind = document.getElementById("email").value
+// 	if (charFind.indexOf("@") == -1) {
+//     	$('div#error_message').append("- Your email should contain an '@' symbol.<br>");
+// 	};
+// 	if (charFind.indexOf(".") == -1) {
+//     	$('div#error_message').append("- Your email should contain an '.' symbol.<br>");
+// 	};
+
+//   });
+// });
+
+// $(document).ready(function() {
+//   $(':submit').click(function() {
+//   	if (document.getElementById("email").value == "") {
+//     	$('div#error_message').append("Enter your email address.");
+// 	};
+// 	if (document.getElementById("password").value == "") {
+//     	$('div#error_message').append("Enter your password.");
+// 	};
+// 	if (document.getElementById("email").value.length < 8) {
+//     	$('div#error_message').append("Your email should be at least 8 characters.");
+// 	};
+// 	if (document.getElementById("password").value.length < 8) {
+//     	$('div#error_message').append("Your password should be at least 8 characters.");
+// 	};
+// 	var charFind = document.getElementById("email").value
+// 	if (charFind.indexOf("@") == -1) {
+//     	$('div#error_message').append("Your email should contain an '@' symbol.");
+// 	};
+//   });
+
+// });
+
