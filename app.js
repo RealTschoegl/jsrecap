ef14c56df9a916ab22b26a031160baba3f9bed6f
MakerSquare Student <students@themakersquare.com>
2013-07-22 17:42:21 -0500

    Complete JS exercise

--- app.js
+++ app.js
@@ -1,81 +1,66 @@
-$(document).ready(function() {
-  $(':submit').click(function() {
-  	$('div#error_message').empty();
-  	$('div#success_message').empty();
-  	var charFind = document.getElementById("email").value
-  	event.preventDefault();
-  	if (document.getElementById("email").value == "") {
-    	$('div#error_message').append("- Enter your email address.<br>");
-	   } 
-	  if (document.getElementById("password").value == "") {
-    	$('div#error_message').append("- Enter your password.<br>");
-	   } 
-	  if (document.getElementById("email").value.length < 8) {
-    	$('div#error_message').append("- Your email should be at least 8 characters.<br>");
-	   } 
-	  if (document.getElementById("password").value.length < 8) {
-    	$('div#error_message').append("- Your password should be at least 8 characters.<br>");
-	   } 
-	  if (charFind.indexOf("@") == -1) {
-    	$('div#error_message').append("- Your email should contain an '@' symbol.<br>");
-	   }  
-	  if (charFind.indexOf(".") == -1) {
-    	$('div#error_message').append("- Your email should contain an '.' symbol.<br>");
-	   }  
-    return true
-    if (return = true) {
-      $('div#success_message').append("Your email has been sent!");
-    } 
-  });
-  
-});
-
 // $(document).ready(function() {
+  // exercise 35
+  // $('.main.card h1').click(function(){
+  //   alert("Weinerschnitzel");
+  // });
+ // exercise 36
+  // $('.main.card h1').click(function(){
+  //   $('.main.card h1').append("!");
+  // });
+ // exercise 37
+  // $('body').click(function() {
+  //   $('body').css("background-color","blue");
+  // });
+ // exercise 38
+  // $('.main.card').click(function(){
+  //   var georgeCloney = $('.main.card').clone();
+  //   $('.main.card').after(georgeCloney);
+  // });
+ // exercise 39 is incomplete 
+  // $('.main.card').click(function(){
+  //   $('body').click(function(){
+  //     $('img.profilepic').hide();
+  //   });
+  // });
+// exercise 40
+  // $('img.profilepic').mousedown(function(){
+  //   $('img.profilepic').hide();
+  // });
+  // $('img.profilepic').mouseup(function(){
+  //   $('img.profilepic').hide();
+  // });
+  // $('img.profilepic').mouseover(function(){
+  //   $('img.profilepic').hide();
+  // });
+  // $('img.profilepic').mouseout(function(){
+  //   $('img.profilepic').hide();
+  // });
+// exercise 41
 //   $(':submit').click(function() {
 //   	$('div#error_message').empty();
+//   	$('div#success_message').empty();
+//   	var charFind = document.getElementById("email").value
 //   	event.preventDefault();
 //   	if (document.getElementById("email").value == "") {
 //     	$('div#error_message').append("- Enter your email address.<br>");
-// 	};
-// 	if (document.getElementById("password").value == "") {
+// 	   } 
+// 	  if (document.getElementById("password").value == "") {
 //     	$('div#error_message').append("- Enter your password.<br>");
-// 	};
-// 	if (document.getElementById("email").value.length < 8) {
+// 	   } 
+// 	  if (document.getElementById("email").value.length < 8) {
 //     	$('div#error_message').append("- Your email should be at least 8 characters.<br>");
-// 	};
-// 	if (document.getElementById("password").value.length < 8) {
+// 	   } 
+// 	  if (document.getElementById("password").value.length < 8) {
 //     	$('div#error_message').append("- Your password should be at least 8 characters.<br>");
-// 	};
-// 	var charFind = document.getElementById("email").value
-// 	if (charFind.indexOf("@") == -1) {
+// 	   } 
+// 	  if (charFind.indexOf("@") == -1) {
 //     	$('div#error_message').append("- Your email should contain an '@' symbol.<br>");
-// 	};
-// 	if (charFind.indexOf(".") == -1) {
+// 	   }  
+// 	  if (charFind.indexOf(".") == -1) {
 //     	$('div#error_message').append("- Your email should contain an '.' symbol.<br>");
-// 	};
-
-//   });
+// 	   }
+//      if ($('div#error_message').html() == "") {
+//         $('#success_message').append("- Your login is complete.")
+//      }
+//   }); 
 // });
-
-// $(document).ready(function() {
-//   $(':submit').click(function() {
-//   	if (document.getElementById("email").value == "") {
-//     	$('div#error_message').append("Enter your email address.");
-// 	};
-// 	if (document.getElementById("password").value == "") {
-//     	$('div#error_message').append("Enter your password.");
-// 	};
-// 	if (document.getElementById("email").value.length < 8) {
-//     	$('div#error_message').append("Your email should be at least 8 characters.");
-// 	};
-// 	if (document.getElementById("password").value.length < 8) {
-//     	$('div#error_message').append("Your password should be at least 8 characters.");
-// 	};
-// 	var charFind = document.getElementById("email").value
-// 	if (charFind.indexOf("@") == -1) {
-//     	$('div#error_message').append("Your email should contain an '@' symbol.");
-// 	};
-//   });
-
-// });
-
